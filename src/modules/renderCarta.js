2d57e8e0c3f081871b273da51b42813402305c18
skeptikos <>
2026-03-03 16:50:00 -0300

    changing links again

--- src/modules/renderCarta.js
+++ src/modules/renderCarta.js
@@ -7,7 +7,7 @@
 export const renderCarta = (carta,divCartas)=>{
 
   const imgCarta = document.createElement("img");
-  imgCarta.src = `/assets/cartas/${carta}.png`; //3H, JD
+  imgCarta.src = `./assets/cartas/${carta}.png`; //3H, JD
   imgCarta.classList.add("carta");
   divCartas.append(imgCarta);
 };
